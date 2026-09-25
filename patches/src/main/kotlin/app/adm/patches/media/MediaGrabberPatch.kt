f963b4621deea78a59373164c79ea9c1fe9a4876
karim <abdelkarimdjelalda@gmail.com>
2026-09-25 19:37:05 +0100

    fix: use explicit registers in media hooks

--- patches/src/main/kotlin/app/adm/patches/media/MediaGrabberPatch.kt
+++ patches/src/main/kotlin/app/adm/patches/media/MediaGrabberPatch.kt
@@ -19,24 +19,24 @@ val mediaGrabberPatch = bytecodePatch(
     execute {
         WebPageStartedFingerprint.method.addInstructions(
             0,
-            "invoke-static {p1, p2}, $EXTENSION_CLASS;->onPageStarted(Landroid/webkit/WebView;Ljava/lang/String;)V"
+            "invoke-static {v4, v5}, $EXTENSION_CLASS;->onPageStarted(Landroid/webkit/WebView;Ljava/lang/String;)V"
         )
         WebRequestFingerprint.method.addInstructions(
             0,
-            "invoke-static {p1, p2}, $EXTENSION_CLASS;->onRequest(Landroid/webkit/WebView;Ljava/lang/String;)V"
+            "invoke-static {v4, v5}, $EXTENSION_CLASS;->onRequest(Landroid/webkit/WebView;Ljava/lang/String;)V"
         )
         WebCreateOptionsMenuFingerprint.let { fingerprint ->
             listOf(411, 293, 140, 60, 19).forEach { index ->
                 fingerprint.method.addInstructions(
                     index,
-                    "invoke-static {p0, p1}, $EXTENSION_CLASS;->onCreateOptionsMenu(Ljava/lang/Object;Landroid/view/Menu;)V"
+                    "invoke-static {v16, v17}, $EXTENSION_CLASS;->onCreateOptionsMenu(Ljava/lang/Object;Landroid/view/Menu;)V"
                 )
             }
         }
         WebOptionsItemSelectedFingerprint.method.addInstructions(
             0,
             """
-                invoke-static {p0, p1}, $EXTENSION_CLASS;->onOptionsItemSelected(Ljava/lang/Object;Landroid/view/MenuItem;)Z
+                invoke-static {v16, v17}, $EXTENSION_CLASS;->onOptionsItemSelected(Ljava/lang/Object;Landroid/view/MenuItem;)Z
                 move-result v0
                 if-eqz v0, :morphe_media_continue
                 const/4 v0, 0x1
